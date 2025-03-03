560958e317c7b5fbf7a0b5d330f9cf99ed146fc1
Supernova4297 <andy.gable2@gmail.com>
2025-03-02 20:05:07 -0500

    Cosmetic Changes

--- src/tutelTest.py
+++ src/tutelTest.py
@@ -1,5 +1,6 @@
 import turtle as tutel
 b = tutel.Turtle()
+b.shape("turtle")
 b.left(180) # heading 0
 b.back(12)
 b.left(180) # heading 180
@@ -13,4 +14,5 @@ b.right(135) # heading 135
 b.forward(16.97) 
 b.left(288.435) # heading -153.435
 b.back(13.8)
+b.stamp()
 input("Goal One: Complete! ")
