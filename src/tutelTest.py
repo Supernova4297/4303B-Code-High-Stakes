137d05edec18b6c74f5e8ba9e0f54384f40074b0
Supernova4297 <andy.gable2@gmail.com>
2025-03-02 19:59:33 -0500

    Tutel!

--- src/tutelTest.py
+++ src/tutelTest.py
@@ -1,9 +1,16 @@
 import turtle as tutel
 b = tutel.Turtle()
-b.left(180)
-b.backward(12)
-b.left(180)
+b.left(180) # heading 0
+b.back(12)
+b.left(180) # heading 180
 b.forward(24)
-b.left(90)
+b.left(90) # heading 90
 b.forward(24)
-b.right(90)
+b.left(90) # heading 0
+b.forward(24)
+b.forward(12)
+b.right(135) # heading 135
+b.forward(16.97) 
+b.left(288.435) # heading -153.435
+b.back(13.8)
+input("")
