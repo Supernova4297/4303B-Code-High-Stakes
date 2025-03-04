9aed0c5889800d06a46a0cd9d921b073990943fc
Supernova4297 <andy.gable2@gmail.com>
2025-03-03 19:31:32 -0500

    Goal 2 w/ Tutel

--- src/tutelTest.py
+++ src/tutelTest.py
@@ -15,11 +15,27 @@ b.forward(16.97)
 b.left(288.435) # heading -153.435
 b.back(13.8)
 b.dot() # Marks the end of the first goal
-input("Goal One: Complete! ")
+
 b.color("red")
 b.forward(14.333)
 b.right(333.435) # heading 180
 b.forward(48)
-b.left(90)
+b.left(90) # heading 90
 b.dot("#FFD700") # Marks Lady Brown Usage
+b.right(146.31) # heading -123.69
+b.forward(43.2666)
+b.left(11.31) # heading -135
+b.forward(33.9411255)
+b.right(90) # heading -45
+b.forward(33.9411255)
+b.left(45) # heading -90
+b.forward(24)
+b.left(90) # heading 180
+b.forward(24)
+b.forward(12)
+b.right(135) # heading -45
+b.forward(16.97)
+b.left(296.565) # heading -161.565
+b.back(13.8)
+b.dot() # End of Goal 2
 input("This is the end! ")
