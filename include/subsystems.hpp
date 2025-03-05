7d7609a9dbef7f2a1295e78bb46502b83e91ccd1
Supernova4297 <andy.gable2@gmail.com>
2025-03-05 11:28:39 -0500

    Fixed it!

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -13,7 +13,7 @@ inline pros::adi::DigitalOut Doinker(1, false);
 // Intake Code
 //pros::Motor Intake (19, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
 inline pros::Motor IntakeA(-20);
-inline pros::Motor IntakeSmall(-13);
+inline pros::Motor IntakeSmall(13);
 // LB Code
 // 5.5 W Config
 inline pros::Motor LadyBrown(-2);
